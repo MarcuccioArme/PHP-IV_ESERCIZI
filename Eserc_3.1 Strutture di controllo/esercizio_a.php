b44ed2f8e4af6a3e3b2c251ad8ddcc647dd958bd
MarcuccioArme <121099587+MarcuccioArme@users.noreply.github.com>
2023-10-10 09:24:14 +0200

    Aggiunto primo esercizio

---
+++ Eserc_3.1 Strutture di controllo/esercizio_a.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tavola Pitagorica Armenise</title>
+</head>
+<body>
+
+    <h1>Tavola Pitagorica</h1>
+
+    <?php
+        echo "<table border=1>";
+        for ($i=1; $i<=10; $i++) {
+            echo "<tr>";
+            for ($j=1; $j<=10; $j++) {
+                echo "<td>".($i*$j)."</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</table>"
+    ?>
+
+</body>
+</html>
